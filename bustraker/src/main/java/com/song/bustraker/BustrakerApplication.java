2adb4db3beeeb47e238ad9e1f5d501a7f218cdab
songY <syj72331@gmail.com>
2025-09-02 16:24:17 +0900

    1

---
+++ bustraker/src/main/java/com/song/bustraker/BustrakerApplication.java
@@ -0,0 +1,13 @@
+package com.song.bustraker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BustrakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BustrakerApplication.class, args);
+	}
+
+}
